c7d8d1452c45c50203c0c4a795d88e9654c87a74
Wh0004mmm <138298790+Wh0004mmm@users.noreply.github.com>
2023-09-23 17:08:18 +0700

    Add files via upload

---
+++ iting.py
@@ -0,0 +1,32 @@
+# This is a Variable 
+hello = 'Hello Everyone'
+print(hello)
+
+# This is a input
+print('Silahkan Ketikan Nama Anda :')
+nama = input()
+
+print(f'Hello {nama}, Selamat Datang Di Project Python ./Wh0004mmm')
+
+# This is a input
+print('Silahkan Pilih Angka [1/5] :')
+angka = input()
+
+if angka == "1"
+  print('LU TOLOL')
+  
+elif angka == "2"
+  print('ANJING LU')
+  
+elif angka == "3"
+  print('KONTOL LU')
+  
+elif angka == "4"
+  print('BANGSAT LU, ANJING KONTOL NGENTOD BUAT LU')
+  
+elif angka == "5"
+  print('NEXT')
+  
+else:
+  print('MASUKIN ANGKA YANG BENER KONTOL')
+  
